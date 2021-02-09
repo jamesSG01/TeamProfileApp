15ae753073f8c5f9840af983e7aa70f6b3722470
Donovan Hill <hilld13@mytru.ca>
2021-02-08 18:31:02 -0800

    fixed some labelling in strings.xml and activity_don_profile.xml... again

--- app/src/main/java/com/example/teamprofile/DonProfile.java
+++ app/src/main/java/com/example/teamprofile/DonProfile.java
@@ -3,6 +3,7 @@ package com.example.teamprofile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DonProfile extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class DonProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_don_profile);
+        ((TextView)findViewById(R.id.dh_textView)).setText("Donovan Hill");
+        ((TextView)findViewById(R.id.dh_program_of_study)).setText("B.Sc (Computer Science)");
+        ((TextView)findViewById(R.id.dh_fun_facts)).setText("I love the word \"Flammenwerfer\"");
+        ((TextView)findViewById(R.id.dh_hobbies)).setText("Many");
+        ((TextView)findViewById(R.id.dh_other_information)).setText("Other");
     }
 }
