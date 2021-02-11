41a6f7cfb40d89488878dd1ffd09e69601a3def1
SpartanI117 <56614496+SpartanI117@users.noreply.github.com>
2021-02-10 21:22:22 -0800

    Finished my profile activity. Added my image as well. I also added a background to mine and don's profile, as well as the main activity, and three buttons on the main activity page. My button is finished and leads to my profile too.

--- app/src/main/java/com/example/teamprofile/IsaiahProfile.java
+++ app/src/main/java/com/example/teamprofile/IsaiahProfile.java
@@ -1,6 +1,21 @@
 package com.example.teamprofile;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.graphics.Matrix;
+import android.widget.ImageView;
 
 public class IsaiahProfile extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isaiah_profile);
+        ((TextView)findViewById(R.id.is_name)).setText("Isaiah Sarkor");
+    }
+
+    public void rotate_pic(View view){
+        ((ImageView)findViewById(R.id.isaiah_image)).setRotation(90);
+    }
 }
