03f488c1a2f92609f21a11c9a8a89ef4ff3607f4
Donovan Hill <hilld13@mytru.ca>
2021-02-10 23:59:43 -0800

    Changed some displayed text. My photo is not showing up in the emulator at this point.

--- app/src/main/java/com/example/teamprofile/DonProfile.java
+++ app/src/main/java/com/example/teamprofile/DonProfile.java
@@ -14,7 +14,7 @@ public class DonProfile extends AppCompatActivity {
         ((TextView)findViewById(R.id.dh_textView)).setText("Donovan Hill");
         ((TextView)findViewById(R.id.dh_program_of_study)).setText("B.Sc (Computer Science)");
         ((TextView)findViewById(R.id.dh_fun_facts)).setText("I love the word \"Flammenwerfer\"");
-        ((TextView)findViewById(R.id.dh_hobbies)).setText("Many");
-        ((TextView)findViewById(R.id.dh_other_information)).setText("Other");
+        ((TextView)findViewById(R.id.dh_hobbies)).setText("Antique Radio Restoration\nAmateur Radio");
+        ((TextView)findViewById(R.id.dh_other_information)).setText("Get off my Lawn.");
     }
 }
